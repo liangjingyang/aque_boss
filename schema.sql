b935dfc12375ad8f5e24de3351f6e84e45de6d74
simplel <simple.continue@gmail.com>
2015-04-08 15:39:57 +0800

    no absolute url

--- schema.sql
+++ schema.sql
@@ -12,12 +12,12 @@ create table nav (
   url text not null
 );
 
-INSERT INTO nav ('name', 'url') VALUES ('帮助', 'http://192.168.1.2');
-INSERT INTO nav ('name', 'url') VALUES ('自动发布', 'http://192.168.1.2/public');
+INSERT INTO nav ('name', 'url') VALUES ('帮助', '/');
+INSERT INTO nav ('name', 'url') VALUES ('自动发布', '/public');
 INSERT INTO nav ('name', 'url') VALUES ('百度', 'http://www.baidu.com');
 INSERT INTO nav ('name', 'url') VALUES ('文档svn', 'svn://192.168.1.2/doc');
 INSERT INTO nav ('name', 'url') VALUES ('资源svn', 'svn://192.168.1.2/element');
-INSERT INTO nav ('name', 'url') VALUES ('数据分析', 'http://192.168.1.2/analysis');
+INSERT INTO nav ('name', 'url') VALUES ('数据分析', '/analysis');
 
 drop table if exists analysis_menu;
 create table analysis_menu (
@@ -28,10 +28,10 @@ create table analysis_menu (
   sort_id integer not null
 );
 
-INSERT INTO analysis_menu ('name', 'url', 'group_id', 'sort_id') VALUES ('货币和积分', 'http://192.168.1.2/analysis', '2', '0');
-INSERT INTO analysis_menu ('name', 'url', 'group_id', 'sort_id') VALUES ('金币', 'http://192.168.1.2/analysis_gold', '2', '1');
-INSERT INTO analysis_menu ('name', 'url', 'group_id', 'sort_id') VALUES ('钻石', 'http://192.168.1.2/analysis_diamond', '2', '2');
-INSERT INTO analysis_menu ('name', 'url', 'group_id', 'sort_id') VALUES ('道具', 'http://192.168.1.2/analysis', '3', '0');
-INSERT INTO analysis_menu ('name', 'url', 'group_id', 'sort_id') VALUES ('道具查询', 'http://192.168.1.2/analysis_item', '3', '1');
-INSERT INTO analysis_menu ('name', 'url', 'group_id', 'sort_id') VALUES ('玩家', 'http://192.168.1.2/analysis', '1', '0');
-INSERT INTO analysis_menu ('name', 'url', 'group_id', 'sort_id') VALUES ('玩家等级', 'http://192.168.1.2/analysis_lv', '1', '1');
+INSERT INTO analysis_menu ('name', 'url', 'group_id', 'sort_id') VALUES ('货币和积分', '/analysis', '2', '0');
+INSERT INTO analysis_menu ('name', 'url', 'group_id', 'sort_id') VALUES ('金币', '/analysis_gold', '2', '1');
+INSERT INTO analysis_menu ('name', 'url', 'group_id', 'sort_id') VALUES ('钻石', '/analysis_diamond', '2', '2');
+INSERT INTO analysis_menu ('name', 'url', 'group_id', 'sort_id') VALUES ('道具', '/analysis', '3', '0');
+INSERT INTO analysis_menu ('name', 'url', 'group_id', 'sort_id') VALUES ('道具查询', '/analysis_item', '3', '1');
+INSERT INTO analysis_menu ('name', 'url', 'group_id', 'sort_id') VALUES ('玩家', '/analysis', '1', '0');
+INSERT INTO analysis_menu ('name', 'url', 'group_id', 'sort_id') VALUES ('玩家等级', '/analysis_lv', '1', '1');
