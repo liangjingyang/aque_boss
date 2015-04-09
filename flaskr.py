7c7cedf9805a30e47217bdd738fea25f9bae4baf
= <=>
2015-04-09 14:42:19 +0800

    op:

--- flaskr.py
+++ flaskr.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import sqlite3
+import MySQLdb 
 import os
 import commands
 import subprocess
@@ -11,34 +12,59 @@ from flask import Flask, request, session, g, redirect, url_for, \
 	abort, render_template, flash
 
 # configuration
-DATABASE = '/tmp/flask.db'
+DATABASE = 'sqlite.db'
 DEBUG = True
 SECRET_KEY = 'development key'
 USERNAME = 'admin'
 PASSWORD = 'default'
+MYSQL_HOST = 'rdsbiazrmbiazrm.mysql.rds.aliyuncs.com'
+MYSQL_PORT = 3306
+MYSQL_USER = 'croods'
+MYSQL_PASSWD = 'croodsmysql'
+MYSQL_DATABASE = 'croods_action_log'
+
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 app.config.from_envvar('FLASHKEY_SETTINGS', silent=True)
 
-def connect_db():
-    return sqlite3.connect(app.config['DATABASE'])
+def connect_mysql():
+    g.mysql_conn = MySQLdb.connect(
+            host=app.config['MYSQL_HOST'], 
+            user=app.config['MYSQL_USER'], 
+            passwd=app.config['MYSQL_PASSWD'])
+    g.mysql_cur = g.mysql_conn.cursor()
+
+def close_mysql():
+    mysql_cur = getattr(g, 'mysql_cur', None)
+    if mysql_cur is not None:
+        mysql_cur.close()
+    mysql_conn = getattr(g, 'mysql_conn', None)
+    if mysql_conn is not None:
+        mysql_conn.close()
+
+def connect_sqlit():
+    g.sqlit = sqlite3.connect(app.config['DATABASE'])
+
+def close_sqlit():
+    sqlit = getattr(g, 'sqlit', None)
+    if sqlit is not None:
+        sqlit.close()
 
 def init_db():
-    with closing(connect_db()) as db:
+    with closing(connect_sqlit()) as db:
 	with app.open_resource('schema.sql', mode='r') as f:
 	    db.cursor().executescript(f.read())
 	db.commit()
 
 @app.before_request
 def before_request():
-    g.db = connect_db()
+    pass
 
 @app.teardown_request
 def teardown_request(exception):
-    db = getattr(g, 'db', None)
-    if db is not None:
-	db.close()
+    close_mysql()
+    close_sqlit()
 
 #@app.route('/')
 #def show_entries():
@@ -48,13 +74,15 @@ def teardown_request(exception):
 
 @app.route('/')
 def nav():
-    cur = g.db.execute('select id, name, url from nav')
+    connect_sqlit()
+    cur = g.sqlit.execute('select id, name, url from nav')
     nav = [dict(id=row[0], name=row[1], url=row[2]) for row in cur.fetchall()]
     return render_template('nav.html', nav=nav)
 
 @app.route('/analysis')
 def analysis():
-    cur = g.db.execute('select id, name, url, group_id, sort_id from analysis_menu')
+    connect_sqlit()
+    cur = g.sqlit.execute('select id, name, url, group_id, sort_id from analysis_menu')
     menu = [dict(id=row[0], name=row[1], url=row[2], group_id=row[3], sort_id=row[4]) for row in cur.fetchall()]
     sorted_menu = sorted(menu, key=lambda menu : menu['sort_id'])  
     sorted_menu2 = sorted(sorted_menu, key=lambda sorted_menu : sorted_menu['group_id'])  
@@ -64,7 +92,33 @@ def analysis():
 
 @app.route('/analysis_gold')
 def analysis_gold():
-    return render_template('analysis_gold.html')
+    connect_mysql()
+    g.mysql_conn.select_db(app.config['MYSQL_DATABASE'])
+    g.mysql_cur.execute('select * from log_money_2015_04')
+    logs = [dict(
+        id=row[0], 
+        sec=row[1], 
+        player_id=row[2], 
+        player_lv=row[3], 
+        action=row[4],
+        type_id=row[5],
+        num=row[6],
+        reason=row[7]
+        ) 
+        for row in g.mysql_cur.fetchall()]
+    logs[0:0] = [dict(
+        id="id",
+        sec="sec",
+        player_id="player_id",
+        player_lv="player_lv",
+        action="action",
+        type_id="type_id",
+        num="num",
+        reason="reason"
+        )]
+    for i in range(len(logs)):
+        logs[i]['tmp_id'] = i
+    return render_template('analysis_gold.html', logs=logs)
 
 @app.route('/analysis_diamond')
 def analysis_diamond():
